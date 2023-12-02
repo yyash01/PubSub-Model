27cf1afaef1a16516e8c6a49daa24b8f1440ae53
Yash Jain <54287899+yyash01@users.noreply.github.com>
2023-12-02 17:47:44 +0530

    Update publisher.js

--- publisher/publisher.js
+++ publisher/publisher.js
@@ -2,6 +2,6 @@ import { fireEvent } from 'c/pubSubConnector';
 
 
 export const publisherFunc = (pageRef, data) => {
-    console.log('Hi JS publisherFunc');
+    console.log('Hi JS yyash01 Added');
     fireEvent(pageRef, 'EventFromPublisher', data);
 };
